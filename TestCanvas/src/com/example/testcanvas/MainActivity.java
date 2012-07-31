cba1cede319b0134acc95dcba4b292b3b9fcf835
Chris Harrison <C.Harrison1337@gmail.com>
2012-07-31 18:33:47 +0800

    Second commit (from Eclipse IDE)

--- TestCanvas/src/com/example/testcanvas/MainActivity.java
+++ TestCanvas/src/com/example/testcanvas/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
+		//poop lol
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
