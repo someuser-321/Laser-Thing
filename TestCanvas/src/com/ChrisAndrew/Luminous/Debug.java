3d9fe9e40ee1d840fa0092159f93265fff74f0b1
Chris Harrison <C.Harrison1337@gmail.com>
2012-09-04 21:55:41 +0800

    Added more buttons.

--- TestCanvas/src/com/ChrisAndrew/Luminous/Debug.java
+++ TestCanvas/src/com/ChrisAndrew/Luminous/Debug.java
@@ -3,7 +3,7 @@ package com.ChrisAndrew.Luminous;
 
 public class Debug {
 	
-	static private boolean debug = true;
+	static private boolean debug = false;
 	
 	static public void log(String msg){
 		if (debug) System.out.println(msg);
